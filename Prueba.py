90f140c49f8e69e6cf85086fd24aed1411904199
JonnathanJuarez <jua15377@uvg.edu.gt>
2017-09-09 23:57:16 -0600

    generacion de tokens segun entrada

--- Prueba.py
+++ Prueba.py
@@ -1,25 +1,47 @@
+
+
 def isNOmbreDeConjunto(entrada):
     tokenName = "letter"
     letter = "abcdfghijklmnopqrstuvwxyz"
     respuesta = entrada in letter
     if (respuesta):
-        print '<%s, "%s">'% (tokenName, entrada)
+        respuesta = '<%s, "%s">'%(tokenName, entrada)
+       #print respuesta
+    else:
+        respuesta = ""
+    return respuesta
 
 def keywords(palabra):
+    lista = ['if','whie','for']
+    if palabra in lista:
+        return True,lista[lista.index(palabra)]
+    else:
+        return False,""
 
-
-fileName = raw_input("ingrese el archivo a lexear")
+fileName = 'test.txt'#raw_input("ingrese el archivo a lexear")
 infile = open(fileName, 'r')
 
 print('>>> Lectura del fichero linea a linea')
 for line in infile:
-    palabra = ""
-    print (isNOmbreDeConjunto(line))
-    for x in range(len(line)-1):
+    salida = ''
+    palabra = ''
+    #print (isNOmbreDeConjunto(line))
+    for x in range(len(line)):
         letter = line[x]
-        try:
-            if
-        except:
-            print "out of range"
+        #try:
+        if letter != " ":
+            palabra = palabra + letter
+            print palabra
+            isKeyword, kindKeyword = keywords(palabra)
+            if isKeyword == True:
+                salida = salida + '<%s, "%s">'%(kindKeyword, palabra) + ' '
+                palabra = ''
+            else:
+                salida = salida + isNOmbreDeConjunto(letter)
+        else:
+            pass
+        #except e:
+        #print "out of range"
+    print salida
 # Cerramos el fichero.
 infile.close()
